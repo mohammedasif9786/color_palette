227393d05686bc9aae127d925a90a74211187e06
mohammad-anash <anash40678910@gmail.com>
2024-07-25 01:00:52 +0530

    Change Code

--- js/color_pel.js
+++ js/color_pel.js
@@ -1,10 +1,7 @@
-let leftPart= document.querySelector(".fa-home")
-let colorId = document.querySelector("#color")
-leftPart.addEventListener('click',(e)=>{
-    let createELe = document.createElement("span")
-    createELe.style.background="red"
-    createELe.style.width=100%
-    colorId.append(createELe)
-
-})
-
+let leftPart = document.querySelector(".fa-home");
+let colorId = document.querySelector("#color");
+leftPart.addEventListener("click", (e) => {
+  let createELe = document.createElement("span");
+  createELe.style.background = "red";
+  createELe.style.width = 100 % colorId.append(createELe);
+});
