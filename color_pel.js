2255a5e19f74efabbab5426db330b3c0b5086608
Mohammed Asif <asif.jdp@gmail.com>
2024-07-24 23:51:42 +0530

    color-palllete

---
+++ color_pel.js
@@ -0,0 +1,10 @@
+let leftPart= document.querySelector(".fa-home")
+let colorId = document.querySelector("#color")
+leftPart.addEventListener('click',(e)=>{
+    let createELe = document.createElement("span")
+    createELe.style.background="red"
+    createELe.style.width=100%
+    colorId.append(createELe)
+
+})
+
